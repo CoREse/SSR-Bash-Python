8b3807aaf7dbaa8bf26b98a0a3c7a821948c3fc5
CoREse <zzdzhangzzd@hotmail.com>
2017-11-06 18:25:24 +0800

    fix unit bug and show download & upload & all flow

--- show_flow.py
+++ show_flow.py
@@ -5,25 +5,35 @@ f = file("/usr/local/shadowsocksr/mudb.json");
 
 json = json.load(f);
 
-print "用户名\t端口\t已用流量\t流量限制"
+print "用户名\t端口\t下载流量\t上传流量\t总已用流量\t流量限制"
 
 for x in json:
   #Convert Unit To MB
   transfer_enable_int = int(x[u"transfer_enable"])/1024/1024;
   d_int = int(x[u"d"])/1024/1024;
+  u_int = int(x[u"u"])/1024/1024;
+  a_int = int(x[u"d"])+int(x[u"u"]);
+  a_int = a_int/1024/1024;
   transfer_unit = "MB"
   d_unit = "MB"
+  u_unit = "MB"
 
   #Convert Unit To GB For Those Number Which Exceeds 1024MB
   if(transfer_enable_int > 1024):
   	transfer_enable_int = transfer_enable_int/1024
   	transfer_unit = "GB"
-  if(transfer_enable_int > 1024):
+  if(d_int > 1024):
   	d_int = d_int/1024
   	d_unit = "GB"
+  if(u_int > 1024):
+  	u_int = u_int/1024
+  	u_unit = "GB"
+  if(a_int > 1024):
+  	a_int = a_int/1024
+  	a_unit = "GB"
 
   #Print In Format
-  print "%s\t%s\t%d%s\t\t%d%s" %(x[u"user"],x[u"port"],d_int,d_unit,transfer_enable_int,transfer_unit)
+  print "%s\t%s\t%d%s\t\t%d%s\t\t%d%s\t\t%d%s" %(x[u"user"],x[u"port"],d_int,d_unit,u_int,u_unit,a_int,a_unit,transfer_enable_int,transfer_unit)
 
 f.close();
 
